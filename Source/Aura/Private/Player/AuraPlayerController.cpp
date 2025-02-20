ab53989f5de3deddbe468e49bff1622edaa03b2d
Osami's Workshop <Osami@osamiworks.com>
2025-02-20 13:26:54 -0600

    Added enhanced input functions to player controller class and created player controller blueprint

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -3,6 +3,8 @@
 
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+
 
 AAuraPlayerController::AAuraPlayerController()
 {
@@ -29,3 +31,32 @@ void AAuraPlayerController::BeginPlay()
 	SetInputMode(InputModeData);
 	
 }
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent =  CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2d InputAxisVector = InputActionValue.Get<FVector2d>();
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.0f);;
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(Rotation).GetUnitAxis(EAxis::Y);
+
+	if (APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		ControlledPawn->AddMovementInput( ForwardDirection, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput( RightDirection, InputAxisVector.X);
+		
+	}
+}
+
+
