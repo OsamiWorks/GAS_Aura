a9ef8db457562cff1c65814ad2bc4069073ded88
Osami's Workshop <Osami@osamiworks.com>
2025-02-22 16:35:50 -0600

    Initial playerState, attributeSet, and abilitySystemComponents have been added

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,11 @@
+// Copyright Osami following a tutorial by Druid Mechanics
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	NetUpdateFrequency = 100.f;
+	
+	
+}
