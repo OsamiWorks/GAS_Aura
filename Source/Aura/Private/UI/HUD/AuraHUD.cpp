6305407d999e1765ec74d8f255b5fda62e48c7f9
Osami's Workshop <Osami@osamiworks.com>
2025-02-26 11:46:35 -0600

    Added UI contruction to viewport

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,14 @@
+// Copyright Osami following a tutorial by Druid Mechanics
+
+
+#include "UI/HUD/AuraHUD.h"
+#include "UI/Widget/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UAuraUserWidget* Widget = CreateWidget<UAuraUserWidget>(GetWorld(), OverlayWidgetClass);
+	Widget->AddToViewport();
+	
+}
